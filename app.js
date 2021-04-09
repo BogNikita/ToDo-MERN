a1168b4ccbe3d0c705b48f2064e3f84c10c87498
BogNikita <67430306+BogNikita@users.noreply.github.com>
2021-04-09 21:20:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'));
-});
+
 app.use(express.json({extended: true}));
 app.use(cookieParser());
 app.use(router);
